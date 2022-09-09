6ab7ca2f712649a52401d4a68e2497caf48c5291
nathalia <nathaliatroina@gmail.com>
2022-09-09 11:30:06 -0400

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ export default function About() {
                         <div className="about-description">
                             <p>Before becoming a software engineer, I achieved a Bachelor’s and Master’s degrees in Nursing and Auditing. Realizing I had an interest in programming as well, I joined Flatiron school software engineering bootcamp. There, I worked on team projects and discovered the opportunity to create something new and solve problems was the real experience I loved. I am looking forward to exploring front-end roles so that I can help build web applications that can give people better experiences solving their own problems.</p>
                             <p>
-                                At this time, I am open to any junior, internship, or apprenticeship opportunities in frontend developerment and software engineering.
+                                At this time, I am open to any junior, internship, or apprenticeship opportunities in frontend developement and software engineering.
                             </p>
                         </div>
                     </div>
